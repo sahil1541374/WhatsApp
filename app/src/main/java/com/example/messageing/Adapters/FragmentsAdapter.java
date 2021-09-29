b225b2a8bb92251a113d040db233aaa0742b639e
zed <sahiljaryal21@gmail.com>
2021-09-29 02:11:01 -0700

    Final Comit

---
+++ app/src/main/java/com/example/messageing/Adapters/FragmentsAdapter.java
@@ -0,0 +1,52 @@
+package com.example.messageing.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.messageing.Fragments.CallsFragments;
+import com.example.messageing.Fragments.ChatsFragment;
+import com.example.messageing.Fragments.StatusFragment;
+
+public class FragmentsAdapter extends FragmentPagerAdapter {
+    public FragmentsAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+     switch (position){
+         case 0: return new ChatsFragment();
+         case 1: return new StatusFragment();
+         case 2: return new CallsFragments();
+         default:return new ChatsFragment();
+     }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if(position==0){
+            title = "CHATS";
+        }
+        if(position==1){
+            title = "STATUS";
+        }
+        if(position==2){
+            title = "CALLS";
+        }
+        return title;
+    }
+}
